0e876f01b227168aa8bc1396afa9971cf9413aac
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-08-06 10:12:04 +0200

    Implementando Destroy y destroyMultiple para Cebo

--- routes/api.php
+++ routes/api.php
@@ -397,6 +397,7 @@ Route::group(['prefix' => 'v2', 'as' => 'v2.', 'middleware' => ['tenant']], func
             Route::get('cebo-dispatches/facilcom-xlsx', [\App\Http\Controllers\v2\ExcelController::class, 'exportCeboDispatchFacilcom'])->name('export_cebo_dispatches_facilcom');
             Route::get('cebo-dispatches/a3erp-xlsx', [\App\Http\Controllers\v2\ExcelController::class, 'exportCeboDispatchA3erp'])->name('export_cebo_dispatches_a3erp');
             Route::apiResource('cebo-dispatches', V2CeboDispatchController::class);
+            Route::delete('cebo-dispatches', [V2CeboDispatchController::class, 'destroyMultiple']);
             Route::get('labels/options', [LabelController::class, 'options'])->name('labels.options');
             Route::apiResource('labels', LabelController::class);
 
