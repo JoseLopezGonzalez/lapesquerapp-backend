1117f585c1e77194b42d34be6faa4573302dc2cd
Jose-PCBRISAMAR <tijito4@gmail.com>
2026-01-23 17:41:53 +0100

    ACtualizando

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -11,7 +11,7 @@
         <a href="{{ $url ?? tenantSetting('company.website_url') }}" style="display: inline-block; color: white;">
             @php($logoUrl = tenantSetting('company.logo_url_small'))
             @if(!empty($logoUrl) && !empty(trim($slot)))
-                <img src="{{ $logoUrl }}" class="logo" alt="Logo {{ tenantSetting('company.name') }}">
+                <img src="{{ $logoUrl }}" class="logo" alt="Logo {{ tenantSetting('company.name') }}" style="max-width:120px; max-height:80px; width:auto; height:auto;">
             @else
                 {{ $slot }}
             @endif
